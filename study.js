fef8cdda1fa8b84564a0c688b8a80fa253c254a8
AlphaMugisha <tigerkev07@gmail.com>
2026-02-18 20:34:35 +0200

    ad

--- study.js
+++ study.js
@@ -1,1 +1,9 @@
 const fs = require('fs');
+fs.readFile('alpha.txt', 'utf8', fundtion(err, data) {
+    if (err) {
+        console.log('error'.err.message);
+        return;
+    }
+    console.log('File contents:', data);
+}
+)
