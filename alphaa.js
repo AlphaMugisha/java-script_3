595126f549efe3091fac073a8c315186831ef9b6
AlphaMugisha <tigerkev07@gmail.com>
2026-02-23 12:12:17 +0200

    learngin shot

---
+++ alphaa.js
@@ -0,0 +1,10 @@
+const user = {
+    email: "tigerkev07@gmail.com",
+    password: "12345678",
+    name: "Kevin",
+    age: 30,
+    country: "USA",
+}
+for(const item in user){
+    return (item);
+}
