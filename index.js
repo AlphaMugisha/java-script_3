557cf5d4267ed8891b248f462649b390fb90f5fa
AlphaMugisha <tigerkev07@gmail.com>
2026-02-25 14:29:53 +0200

    adding the thing

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+// const port = 3000;
 
 
 
-mongoose.connect('mongodb://localhost:27017/local', )
-.then(() =>console.log("conneted"))
-    .catch((err) => {console.error("connection error", err)});
+// mongoose.connect('mongodb://localhost:27017/local', )
+// .then(() =>console.log("conneted"))
+//     .catch((err) => {console.error("connection error", err)});
 
-app.get('/users', (req, res) => {
-    res.json({message: "server is active"});
-});
-
-
-// app.middleware((req, res, next) => {
-//   console.log(`${req.method} ${req.url}`);
-//   next();
+// app.get('/users', (req, res) => {
+//     res.json({message: "server is active"});
 // });
 
-function logmid(req, res, next) {
-  console.log(`${req.method} ${req.url}`);
-  next();
-}
 
-app.use(logmid);
+// // app.middleware((req, res, next) => {
+// //   console.log(`${req.method} ${req.url}`);
+// //   next();
+// // });
 
-app.get('/users', (req, res) => {
-  res.send('we are getting all users!');
-});
+// function logmid(req, res, next) {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// }
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// app.use(logmid);
 
-app.get('/users', (req, res) => {
-  res.json({message: "server is active"});
-}         
-);
+// app.get('/users', (req, res) => {
+//   res.send('we are getting all users!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+// app.get('/users', (req, res) => {
+//   res.json({message: "server is active"});
+// }         
+// );
+import connectDB from "./config/db.js";
+connectDB();
+ 
