904ae58b4552943bb3f04655009f2b53e5bc3dc6
AlphaMugisha <tigerkev07@gmail.com>
2026-03-02 13:55:51 +0200

    adding that part

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,23 +1,64 @@
-import user from '..models/user.js';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();    
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const register = async (req, res) => {
-    try {
-        const {name, email, password} = req.body;
-        const existig = await user.findOne({email});
 
-        if(existig){
-            return res.status(400).json({message: "User already exists"});
-        }
+// REGISTER
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const createduser = await user.creat ({name, email, password: hashedPassword});
-        const token = jwt.sign({email: createduser.email, id: createduser._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
-        res.status(201).json({user: createduser, token});
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+    if (!name || !email || !password)
+      return res.status(400).json({ message: "Missing fields" });
+
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({ message: "User registered successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// LOGIN
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ message: "Missing fields" });
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user._id },
+      "yourSecretKey",
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
