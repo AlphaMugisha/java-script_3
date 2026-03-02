bc35fa8414176162722c575693d931b5db93437f
AlphaMugisha <tigerkev07@gmail.com>
2026-03-02 08:42:34 +0200

    adding

---
+++ routes/productRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct
+} = require("../controllers/productController");
+
+// MULTER CONFIG
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+// ROUTES
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.post("/", upload.single("image"), createProduct);
+router.put("/:id", upload.single("image"), updateProduct);
+router.delete("/:id", deleteProduct);
+
+module.exports = router;
