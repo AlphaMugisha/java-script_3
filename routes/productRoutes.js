1f15b33f28f1ca3d8cab85df237911f4dc2d9625
AlphaMugisha <tigerkev07@gmail.com>
2026-03-02 11:00:58 +0200

    pushing

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const multer = require("multer");
-
-const {
+import express from "express";
+import multer from "multer";
+import {
   getAllProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct
-} = require("../controllers/productController");
+} from "../controllers/productController.js";
+
+const router = express.Router();
 
 // MULTER CONFIG
 const storage = multer.diskStorage({
@@ -28,7 +28,5 @@ router.get("/:id", getProductById);
 router.post("/", upload.single("image"), createProduct);
 router.put("/:id", upload.single("image"), updateProduct);
 router.delete("/:id", deleteProduct);
-
-module.exports = router;
 
 export default router;
