3844e37a5ec9bbe58f23f30a557b166474da6101
AlphaMugisha <tigerkev07@gmail.com>
2026-03-04 14:22:01 +0200

    adding the thing

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,7 @@ export const getProducts = async (req, res) => {
     const products = await Product.find();
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ messagea: error.message });
   }
 };
 
