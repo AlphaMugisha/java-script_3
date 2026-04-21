c6136152ef8f3ebd9e3afc3577451c0be8b3280f
AlphaMugisha <tigerkev07@gmail.com>
2026-04-21 11:10:57 +0200

    about to add the front end of the supermarket project

---
+++ shop/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div>
+      <h1>Karibu simba supermarket</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et vero ea maiores itaque optio quisquam fugit nemo officia doloribus? Inventore facere corporis, animi quis officia mollitia eos consequatur sequi totam.</p>
+    </div>
+  )
+}
+
+export default App
